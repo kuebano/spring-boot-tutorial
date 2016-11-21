0957db5624641b3e566b31ac2fc054452af331d2
Ezeokeke Kingsley <kezeokeke@seamfix.com>
2016-11-21 10:53:42 +0100

    Fix embedded a spring boot app that enforce web security using ldap

--- src/main/java/kuebano/controller/ldap/Application.java
+++ src/main/java/kuebano/controller/ldap/Application.java
@@ -1,6 +1,5 @@
 package kuebano.controller.ldap;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
@@ -9,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+//    public static void main(String[] args) {
+//        SpringApplication.run(Application.class, args);
+//    }
 }
